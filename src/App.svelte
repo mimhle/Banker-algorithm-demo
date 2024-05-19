c0a4003068a1fd9fbad3ccda720fdfc6006ccaed
mimhle <40392166+mimhle@users.noreply.github.com>
2024-05-19 11:46:07 +0700

    Update README.md

--- src/App.svelte
+++ src/App.svelte
@@ -4,10 +4,7 @@
 
 <main>
   <h1 class="bg-red-500 text-center flex flex-row justify-center">Vite + Svelte</h1>
-
   <div class="w-full flex flex-row justify-center">
     <Table />
   </div>
-
-
 </main>
