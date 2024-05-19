1808f15889a985ddc8205c2bd31573afdb4d91f8
mimhle <40392166+mimhle@users.noreply.github.com>
2024-05-19 13:42:05 +0700

    Update Table.svelte

--- src/App.svelte
+++ src/App.svelte
@@ -1,10 +1,27 @@
 <script>
-  import Table from "./lib/Table.svelte";
+    import Table from "./lib/Table.svelte";
+
+    let testTable1 = [
+        [1, 2, 3],
+        [4, 5, 6],
+        [7, 8, 9]
+    ];
+
+    let testTable2 = testTable1;
+
+    $: {
+        testTable2 = testTable1.map(row => row.map(cell => parseInt(cell.toString()) + 1));
+        console.log(testTable2);
+    }
 </script>
 
 <main>
-  <h1 class="bg-red-500 text-center flex flex-row justify-center">Vite + Svelte</h1>
-  <div class="w-full flex flex-row justify-center">
-    <Table />
-  </div>
+    <h1 class="bg-red-500 text-center flex flex-row justify-center">Vite + Svelte</h1>
+    <div class="w-full flex flex-row justify-center">
+        <Table title="Input" bind:table={testTable1}/>
+    </div>
+    <br>
+    <div class="w-full flex flex-row justify-center">
+        <Table title="Output" disabled table={testTable2}/>
+    </div>
 </main>
--- src/lib/Table.svelte
+++ src/lib/Table.svelte
@@ -1,75 +1,96 @@
 <script>
-    let table = [
-        [0, 0, 0, 0],
-        [0, 0, 0, 0],
-        [0, 0, 0, 0],
-        [0, 0, 0, 1]
-    ]
+    export let title;
+    export let disabled = false;
+
+    export let table = [
+        [0, 0, 0],
+        [0, 0, 0],
+        [0, 0, 0],
+    ];
 
     const addRow = () => {
         table = [...table, Array(table[0].length).fill(0)]
     }
 
-    const removeRow = () => {
+    const removeRow = (at = -1) => {
         if (table.length === 1) {
             return
         }
-        table = table.slice(0, -1)
+        if (at === -1) {
+            at = table.length - 1
+        }
+        table = table.filter((_, i) => i !== at)
     }
 
     const addColumn = () => {
         table = table.map(row => [...row, 0])
     }
 
-    const removeColumn = () => {
+    const removeColumn = (at = -1) => {
         if (table[0].length === 1) {
             return
         }
-        table = table.map(row => row.slice(0, -1))
-    }
-
-    $: {
-        if (table.every(row => row.every(cell => cell !== null))) {
-            console.log(table)
+        if (at === -1) {
+            at = table[0].length - 1
         }
+        table = table.map(row => {
+            row.splice(at, 1)
+            return row
+        })
     }
 
-    const cellWidth = "w-32"
-    const cellHeight = "h-24"
+    const cellWidth = "w-14"
+    const cellHeight = "h-10"
 </script>
 
 <div class="flex flex-col items-center w-fit">
-    <h2 class="w-full flex flex-row justify-center">Table</h2>
+    <h2 class="text-lg font-bold w-full flex flex-row justify-center">{title}</h2>
     <div class="flex flex-row w-fit">
         {#each table[0] as cell, i (i)}
-            <span class="{cellWidth} my-auto flex flex-row justify-center">Column {i + 1}</span>
+            <button class="{cellWidth} my-auto flex flex-col justify-center hover:bg-gray-200 disabled:!bg-inherit"
+                    on:click={() => removeColumn(i)}
+                    {disabled}
+            >
+                R{i + 1}
+            </button>
         {/each}
     </div>
     {#each table as row, i (i)}
         <div class="flex flex-row">
-            <span class="m-3 my-auto flex flex-row justify-center">Row {i + 1}</span>
+            <div class="{cellWidth} {cellHeight} flex flex-col justify-center">
+                <button class="w-full h-full my-auto flex flex-col justify-center hover:bg-gray-200 disabled:!bg-inherit"
+                        on:click={() => removeRow(i)}
+                        {disabled}
+                >
+                    P{i + 1}
+                </button>
+            </div>
             {#each row as cell, j (j)}
-                <div class="{cellWidth} {cellHeight} p-5 border border-black flex flex-col justify-center">
+                <div class="{cellWidth} {cellHeight} border border-black flex flex-col justify-center">
                     <input
                             type="number"
-                            class="w-12 m-auto"
+                            class="w-full h-full text-center"
                             min="0"
                             max="999"
                             on:blur={(e) => {
                                  e.target.value = Math.min(999, Math.max(0, e.target.value))
+                                 table[i][j] = e.target.value
                             }}
                             bind:value={table[i][j]}
+                            {disabled}
                     />
                 </div>
             {/each}
-            <div>
-                <button class="m-3" on:click={addColumn}><i class="fa fa-plus"></i></button>
-                <button class="m-3" on:click={removeColumn}><i class="fa fa-minus"></i></button>
+            <div class="{cellWidth} {cellHeight} flex flex-row justify-center gap-1">
+                <button class="m-0 disabled:opacity-50" on:click={() => addColumn()} {disabled}><i class="fa fa-plus"></i></button>
+                <button class="m-0 disabled:opacity-50" on:click={() => removeColumn()} {disabled}><i class="fa fa-minus"></i></button>
             </div>
         </div>
     {/each}
-    <div class="">
-        <button class="m-3" on:click={addRow}><i class="fa fa-plus-circle fa-lg"></i></button>
-        <button class="m-3" on:click={removeRow}><i class="fa fa-minus-circle fa-lg"></i></button>
-    </div>
+    {#if !disabled}
+        <div>
+            <button class="m-3" on:click={() => addRow()}><i class="fa fa-plus-circle fa-lg"></i></button>
+            <button class="m-3" on:click={() => removeRow()}><i class="fa fa-minus-circle fa-lg"></i></button>
+        </div>
+    {/if}
 </div>
