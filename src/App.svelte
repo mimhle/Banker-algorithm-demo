fae717efcbbea6a0ba95fd675e147442e72dbc4e
mimhle <40392166+mimhle@users.noreply.github.com>
2024-05-21 13:53:25 +0700

    Refactor and added persistent storage

--- src/App.svelte
+++ src/App.svelte
@@ -1,26 +1,27 @@
 <script>
+    import { persisted } from 'svelte-persisted-store'
     import Table from "./lib/Table.svelte";
     import Solver from "./lib/Solver.js";
     import { addColumn, addRow, randomizeTable, removeRow, removeColumn, randomizeNumber } from "./lib/utils.js";
 
-    let available = [
+    const available = persisted('available', [
         [1, 2, 3],
         [4, 5, 6],
         [7, 8, 9],
         [10, 11, 12],
-    ];
+    ]);
 
-    let max = [
+    const max = persisted('max', [
         [1, 2, 3],
         [4, 5, 6],
         [7, 8, 9]
-    ];
+    ]);
 
-    let allocation = [
+    const allocation = persisted('allocation', [
         [1, 2, 3],
         [4, 5, 6],
         [7, 8, 9]
-    ];
+    ]);
 
     let need = [
         [0, 0, 0],
@@ -31,44 +32,44 @@
     let order = [[1], [2], [3]];
     let safeSequence = [-1, -1, -1];
 
-    let xLabels = max[0].map((_, i) => `R${i+1}`);
-    let yLabels = max.map((_, i) => `P${i+1}`);
+    let xLabels = $max[0].map((_, i) => `R${i+1}`);
+    let yLabels = $max.map((_, i) => `P${i+1}`);
 
     const addProcess = () => {
-        max = addRow(max);
-        allocation = addRow(allocation);
+        $max = addRow($max);
+        $allocation = addRow($allocation);
     }
 
     const removeProcess = (i) => {
-        max = removeRow(max, i);
-        allocation = removeRow(allocation, i);
+        $max = removeRow($max, i);
+        $allocation = removeRow($allocation, i);
     }
 
     const addResource = () => {
-        max = addColumn(max);
-        allocation = addColumn(allocation);
-        available = addColumn(available);
+        $max = addColumn($max);
+        $allocation = addColumn($allocation);
+        $available = addColumn($available);
     }
 
     const removeResource = (i) => {
-        max = removeColumn(max, i);
-        allocation = removeColumn(allocation, i);
-        available = removeColumn(available, i);
+        $max = removeColumn($max, i);
+        $allocation = removeColumn($allocation, i);
+        $available = removeColumn($available, i);
     }
 
     const randomize = () => {
-        max = randomizeTable(max, 1, 10);
-        allocation = allocation.map((row, i) => row.map((_, j) => randomizeNumber(0, max[i][j])));
-        available = randomizeTable(available, 5, 10);
+        $max = randomizeTable($max, 1, 10);
+        $allocation = $allocation.map((row, i) => row.map((_, j) => randomizeNumber(0, $max[i][j])));
+        $available = randomizeTable($available, 5, 10);
     }
 
     $: {
-        xLabels = max[0].map((_, i) => `R${i+1}`);
-        yLabels = max.map((_, i) => `P${i+1}`);
+        xLabels = $max[0].map((_, i) => `R${i+1}`);
+        yLabels = $max.map((_, i) => `P${i+1}`);
 
-        let banker = new Solver(available, max, allocation);
+        let banker = new Solver($available, $max, $allocation);
         need = banker.need;
-        available = banker.available;
+        $available = banker.available;
         order = banker.order.map((e) => [e]);
         safeSequence = banker.safeSequence;
     }
@@ -82,10 +83,10 @@
         <button on:click={randomize} class="flex" title="Randomize">
             <i class="fa fa-refresh  fa-lg"></i>
         </button>
-        <Table title="Max" bind:table={max} {xLabels} {yLabels} offset disabled={false} onYLabelsClick={removeProcess} onXLabelsClick={removeResource}/>
-        <Table title="Allocation" bind:table={allocation} yLabel={false} {xLabels} {yLabels} offset disabled={false} onYLabelsClick={removeProcess} onXLabelsClick={removeResource}/>
+        <Table title="Max" bind:table={$max} {xLabels} {yLabels} offset disabled={false} onYLabelsClick={removeProcess} onXLabelsClick={removeResource}/>
+        <Table title="Allocation" bind:table={$allocation} yLabel={false} {xLabels} {yLabels} offset disabled={false} onYLabelsClick={removeProcess} onXLabelsClick={removeResource}/>
         <Table title="Need" bind:table={need} yLabel={false} disabled {xLabels} {yLabels} offset onYLabelsClick={removeProcess} onXLabelsClick={removeResource}/>
-        <Table title="Available" bind:table={available} {xLabels} yLabel={false} disabled={available.map((_, i) => i !== 0)} onXLabelsClick={removeResource}/>
+        <Table title="Available" bind:table={$available} {xLabels} yLabel={false} disabled={$available.map((_, i) => i !== 0)} onXLabelsClick={removeResource}/>
         <Table title="&#8288;" bind:table={order} xLabels={["Nₒ."]} yLabel={false} disabled offset/>
         <button on:click={addResource} class="flex" title="Add Resource">
             <i class="fa fa-plus fa-lg"></i>
@@ -103,6 +104,6 @@
         </button>
     </div>
     <div class="flex flex-row justify-center gap-3 my-3">
-        <Table title="Safe sequence" table={[safeSequence.map((v, _) => xLabels[v])]} yLabel={false} xLabel={false} disabled/>
+        <Table title="Safe sequence" table={[safeSequence.map((v, _) => yLabels[v])]} yLabel={false} xLabel={false} disabled/>
     </div>
 </main>
